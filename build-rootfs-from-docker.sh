605c10de0612ab2ba5583cff37f9b05fe2560c5e
Gregory Boddin <gregory@siwhine.net>
2016-08-11 13:09:15 +0200

    Release rootfs builder for various linux distro

--- build-rootfs-from-docker.sh
+++ build-rootfs-from-docker.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 while (($#)); do 
   TAR=$(echo $1|sed 's/:/-/g').tar
+  echo "Pulling distro $1 from docker registry"
   docker pull $1
+  echo "Creating docker container from $1"
   ID=$(docker create $1)
+  echo "Exporting docker container $ID to $TAR ..."
   docker export -o "$TAR" "$ID"
+  echo "Compressing $TAR ..."
   bzip2 $TAR
+  echo "Generating hashes for ${TAR}.bz2"
   md5sum ${TAR}.bz2 > ${TAR}.bz2.md5
   sha1sum ${TAR}.bz2 > ${TAR}.bz2.sha1
   sha256sum ${TAR}.bz2 > ${TAR}.bz2.sha256
+  echo "${TAR}.bz2 built"
   shift
 done
